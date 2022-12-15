30f5be17bf47c24ea866527a51d70b720a3a9509
Nnamani John <nnamanijoh123@gmail.com>
2022-12-15 15:55:03 +0000

    pchar

--- inst_3.c
+++ inst_3.c
@@ -62,3 +62,29 @@ void mod_stack(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+
+/**
+ * pchar_stack - prints char at the top of the stack
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void pchar_stack(stack_t **stack, unsigned int line_number)
+{
+	if (stack && *stack)
+		if ((*stack)->n >= 0 && (*stack)->n <= 177)
+			printf("%c\n", (*stack)->n);
+		else
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
